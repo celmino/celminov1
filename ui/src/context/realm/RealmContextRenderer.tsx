96d8bac3582363ec8f59069f206b1aa42753f314
selimtan <stanoncloud@gmail.com>
2024-03-09 00:44:57 +0300

    .

--- ui/src/context/realm/RealmContextRenderer.tsx
+++ ui/src/context/realm/RealmContextRenderer.tsx
@@ -1,4 +1,4 @@
-import { useGetOrganization } from "@realmocean/sdk";
+import { useGetOrganization, useGetRealm } from "@realmocean/sdk";
 import { is } from "@tuval/core";
 import { useParams } from "@tuval/forms";
 import React from "react";
@@ -10,14 +10,14 @@ import { RealmContextProvider } from "./context";
 function RealmContextRenderer({ control }: { control: RealmContextClass }) {
 
 
-    const { organizationId } = useParams();
-    const { organization, isLoading } = useGetOrganization(organizationId);
+    const { workspaceId } = useParams();
+    const { realm, isLoading } = useGetRealm({ realmId: workspaceId, enabled: true });
 
 
     return (
         is.function(control.vp_ChildFunc) || !isLoading ?
             (
-                <RealmContextProvider.Provider value={organization}>
+                <RealmContextProvider.Provider value={realm}>
 
                     {
                         control.vp_ChildFunc()?.render()
