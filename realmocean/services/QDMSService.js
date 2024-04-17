4f8e9e4c99a958f4950f71e72d82a16f381c034c
selimtan <stanoncloud@gmail.com>
2024-04-17 16:08:03 +0300

    .

--- realmocean/services/QDMSService.js
+++ realmocean/services/QDMSService.js
@@ -1,21 +1,28 @@
 const BaseService = require("../BaseService");
 
 
-const authPack = {
-    url: 'http://93.180.135.42/QDMS/QDMSNET/BSAT/BSATWebapi.asmx?WSDL',
-    token : 'jG5KCJiNA0ßßß7CUAVZ5naWvPKfVFcqex3JKJy$$$fZV2rz5u$$$RDfGWßßßxnRCAlVJDDjkhVnROßßßzSnZ27aQtLxßßß8Bnw==' 
-}
+
+const url = 'http://93.180.135.42/QDMS/QDMSNET/BSAT/BSATWebapi.asmx?WSDL';
+
 
 class QDMSTestService extends BaseService {
     async init() {
         const schedule = this.services.get('schedule-service');
         const qdms = this.services.get('qdms-service');
 
-         schedule.addJob('*/2 * * * * *', async () => {
-            const users = await qdms.getUsers(authPack);
-            console.log(users);
-        }) 
-        
+        const token = await qdms.getToken(url, 'qdms', 'qdms24');
+
+
+        const authPack = {
+            url: 'http://93.180.135.42/QDMS/QDMSNET/BSAT/BSATWebapi.asmx?WSDL',
+            token
+        }
+
+        schedule.addJob('*/2 * * * * *', async () => {
+           // const users = await qdms.getUsers(authPack);
+            console.log(token);
+        })
+
     }
 }
 
